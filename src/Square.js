05421945c672919089fcff83913249945a38baf4
DHYAN PATEL <62758631+dhyanpatel110@users.noreply.github.com>
2021-10-29 20:48:23 +0530

    Add files via upload

---
+++ src/Square.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Square = (props) => {
+    return (
+        <div className={'square'} {...props}>
+            {props.x ? 'x' : (props.o ? 'o' : '')}
+        </div>
+    )
+}
+
+export default Square
